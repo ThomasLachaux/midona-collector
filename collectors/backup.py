ff228a8eb79503243a4d543e8bcba76b9edbe44e
Thomas de Lachaux <thomas.delachaux@gmail.com>
2021-07-04 16:01:51 +0200

    add backup collector and dynamic import

---
+++ collectors/backup.py
@@ -0,0 +1,21 @@
+from subprocess import run, PIPE
+
+import json
+
+def collect():
+  # Retreive stats for the whole repo and the latest snapshot
+  for snapshot in ['', 'latest']:
+    stats = lambda mode : json.loads(run(['restic', 'stats', '--mode', mode, '--json', snapshot], stdout=PIPE, text=True).stdout)
+
+    raw_data = stats('raw-data')
+    restore_size = stats('restore-size')
+
+    # Define the scope name
+    scope = "latest_snapshot"
+    if not snapshot:
+      scope = "repository"
+
+    yield f'backup,mode=raw_data,scope={scope} total_size={raw_data["total_size"]}'
+
+    yield f'backup,mode=restore_size,scope={scope} total_size={restore_size["total_size"]}'
+    yield f'backup,mode=restore_size,scope={scope} total_file_count={restore_size["total_file_count"]}'
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from os import environ
+import pkgutil
+import collectors
+import logging as logger
 
+from os import environ
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-from collectors import flood
-
-# for data in flood.collect():
-#   print(data)
+logger.basicConfig(format='%(asctime)s %(module)s %(levelname)s %(message)s', level=logger.DEBUG)
 
 client = InfluxDBClient(url=environ.get("INFLUXDB_URL"), token=environ.get("INFLUXDB_TOKEN"))
+api = client.write_api(write_options=SYNCHRONOUS)
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
+for importer, module_name, ispkg in pkgutil.iter_modules(collectors.__path__, f'{collectors.__name__}.'):
+  logger.info(f'Collect with {module_name}')
+  collector = __import__(module_name, fromlist=[None])
 
-for data in flood.collect():
-  print(data)
-  write_api.write(environ.get("INFLUXDB_BUCKET"), environ.get("INFLUXDB_ORG"), data)
+  for data in collector.collect():
+    logger.debug(data)
+    api.write(environ.get("INFLUXDB_BUCKET"), environ.get("INFLUXDB_ORG"), data)
