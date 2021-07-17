3dc565c529671409ff3f4915ec3ad077eee18189
Thomas de Lachaux <thomas.delachaux@gmail.com>
2021-07-17 17:09:24 +0200

    change dynamic import to static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import logging as logger
 logger.basicConfig(format='%(levelname)s %(module)s %(message)s', level=logger.DEBUG)
 
-import pkgutil
-import collectors
-
 from os import environ
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -16,12 +13,14 @@ healthcheck.start()
 client = InfluxDBClient(url=environ.get("INFLUXDB_URL"), token=environ.get("INFLUXDB_TOKEN"))
 api = client.write_api(write_options=SYNCHRONOUS)
 
-for importer, module_name, ispkg in pkgutil.iter_modules(collectors.__path__, f'{collectors.__name__}.'):
-  logger.info(f'Collect with {module_name}')
-  collector = __import__(module_name, fromlist=[None])
+for collector_name in environ.get('COLLECTORS').split(','):
+  logger.info(f'Collect with {collector_name}')
+
+  collector = __import__(f'collectors.{collector_name}', fromlist=[None])
 
   for data in collector.collect():
     api.write(environ.get("INFLUXDB_BUCKET"), environ.get("INFLUXDB_ORG"), data)
     logger.debug(data)
+
 
 healthcheck.stop()
